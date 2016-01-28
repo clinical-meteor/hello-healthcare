a91feaabe88d67bf933e99238edc4021587a495e
Abigail Watson <awatson1978@gmail.com>
2016-01-27 23:40:06 -0800

    4 verification tests; all green

--- client/components/homePage/homePage.js
+++ client/components/homePage/homePage.js
@@ -1,15 +1,20 @@
 
+// Router.route('/', {
+//   name: 'homeRoute',
+//   template: 'homePage'
+// });
+
 
 // counter starts at 0
 Session.setDefault('counter', 0);
 
-Template.hello.helpers({
+Template.homePage.helpers({
   counter: function () {
     return Session.get('counter');
   }
 });
 
-Template.hello.events({
+Template.homePage.events({
   'click button': function () {
     // increment the counter when button is clicked
     Session.set('counter', Session.get('counter') + 1);
